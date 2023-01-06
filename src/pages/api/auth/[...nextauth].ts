86fa51cf5d3493561ae775ab2c4863e085dfc740
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-01-06 18:56:06 +0530

    github as  auth

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,6 @@
 import NextAuth, { type NextAuthOptions } from "next-auth";
 import DiscordProvider from "next-auth/providers/discord";
+import GithubProvider from "next-auth/providers/github.js";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 
 import { env } from "../../../env/server.mjs";
@@ -20,8 +21,17 @@ export const authOptions: NextAuthOptions = {
       clientId: env.DISCORD_CLIENT_ID,
       clientSecret: env.DISCORD_CLIENT_SECRET,
     }),
-   
+    GithubProvider({
+      clientId: env.GITHUB_CLIENT_ID,
+      clientSecret: env.GITHUB_CLIENT_SECRET,
+    }),
   ],
+  secret: process.env.NEXUTAUTH_SECRET,
+  pages: {
+    signIn: "/signin",
+    newUser: "/",
+    error: "/",
+  },
 };
 
 export default NextAuth(authOptions);
