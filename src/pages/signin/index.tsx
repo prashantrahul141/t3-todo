86e39f52e2a4fc0b1b412ede6080403dd2d9c85f
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-01-06 18:56:18 +0530

    adding file

---
+++ src/pages/signin/index.tsx
@@ -0,0 +1,12 @@
+import { type NextPage } from "next";
+// import Head from "next/head";
+// import Link from "next/link";
+// import { useSession } from "next-auth/react";
+
+// import { api } from "../utils/api";
+
+const Signin: NextPage = () => {
+  return <div></div>;
+};
+
+export default Signin;
