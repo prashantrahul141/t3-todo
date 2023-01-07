d6bae8702b60fdae5a7e3afa65f251f84db5717b
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-01-07 23:00:53 +0530

    adding /new redirect to /new/note

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,5 @@
 // @ts-check
+
 /**
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
  * This is especially useful for Docker builds.
@@ -28,6 +29,15 @@ const config = {
         pathname: '/u/**',
       },
     ],
+  },
+  async redirects() {
+    return [
+      {
+        source: '/new',
+        destination: '/new/note',
+        permanent: true,
+      },
+    ];
   },
 };
 export default config;
