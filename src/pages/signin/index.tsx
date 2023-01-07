d3fd7ab84afd8c7e329cef39a0b177343573f39e
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-01-07 14:52:02 +0530

    linking main page

--- src/pages/signin/index.tsx
+++ src/pages/signin/index.tsx
@@ -1,19 +1,22 @@
 import { type NextPage } from 'next';
 import CompHead from '@components/common/CompHead';
 import CompSigninForm from '@components/forms/signinform';
+import Link from 'next/link';
 
 const Signin: NextPage = () => {
   return (
     <>
       <CompHead headTitle='Sign in'></CompHead>
-      <div className='absolute top-1/4 left-1/2 w-full -translate-x-1/2 -translate-y-1/2 rounded  p-4 text-center sm:w-96'>
-        <h6 className='font-lato text-xs tracking-wide text-themePrimary-100'>
-          WELCOME TO
-        </h6>
-        <h2 className='font-oswald text-6xl tracking-wide text-themePrimary-400'>
-          T3-TODO
-        </h2>
-      </div>
+      <Link href={'./'}>
+        <div className='absolute top-1/4 left-1/2 w-full -translate-x-1/2 -translate-y-1/2 rounded  p-4 text-center sm:w-96'>
+          <h6 className='font-lato text-xs tracking-wide text-themePrimary-100'>
+            WELCOME TO
+          </h6>
+          <h2 className='font-oswald text-6xl tracking-wide text-themePrimary-400'>
+            T3-TODO
+          </h2>
+        </div>
+      </Link>
       <CompSigninForm></CompSigninForm>
     </>
   );
