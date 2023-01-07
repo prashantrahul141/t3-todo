5b236145babe4df2ddef8b768740e843d416d8f0
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-01-07 14:54:19 +0530

    using FC

--- src/components/forms/signinform.tsx
+++ src/components/forms/signinform.tsx
@@ -1,7 +1,7 @@
-import { type NextPage } from 'next';
 import { signIn } from 'next-auth/react';
+import type { FC } from 'react';
 
-const CompSigninForm: NextPage = () => {
+const CompSigninForm: FC = () => {
   return (
     <div className='absolute left-1/2 top-1/2 w-full -translate-x-1/2 -translate-y-1/2  p-4 sm:w-96'>
       <button className='btn-signin' onClick={() => signIn('github')}>
