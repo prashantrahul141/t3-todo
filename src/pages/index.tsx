7a2eda188213680c08fe36f21557bd8e847213b8
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-01-07 21:17:36 +0530

    using top bar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
-import { type NextPage } from "next";
-import CompHead from "@components/common/CompHead";
+import { type NextPage } from 'next';
+import CompHead from '@components/common/CompHead';
+import TopBar from '@components/common/TopBar';
 // import Head from "next/head";
 // import Link from "next/link";
 // import { useSession } from "next-auth/react";
@@ -9,7 +10,8 @@ import CompHead from "@components/common/CompHead";
 const Home: NextPage = () => {
   return (
     <>
-      <CompHead headTitle="Home"></CompHead>
+      <CompHead headTitle='Home'></CompHead>
+      <TopBar avatarUrl='https://cdn.discordapp.com/avatars/995581232459038760/d37933badeb94c9f07d3fabc103df2f0.png'></TopBar>
     </>
   );
 };
