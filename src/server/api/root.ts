9bc559e18688ca43923ad5f10118ed65b721839d
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-01-08 22:01:11 +0530

    removing example router

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,4 @@
 import { createTRPCRouter } from './trpc';
-import { exampleRouter } from './routers/example';
 import { folderRouter } from './routers/folder';
 
 /**
@@ -8,7 +7,6 @@ import { folderRouter } from './routers/folder';
  * All routers added in /api/routers should be manually added here
  */
 export const appRouter = createTRPCRouter({
-  example: exampleRouter,
   folder: folderRouter,
 });
 
