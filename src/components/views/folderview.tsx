a5b2cd795b6da55a924e3f06dc2143a64ddad503
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-01-09 02:30:00 +0530

    adding comphead and top bar components

--- src/components/views/folderview.tsx
+++ src/components/views/folderview.tsx
@@ -1,15 +1,34 @@
+import CompHead from '@components/common/CompHead';
+import TopBar from '@components/common/TopBar';
+import FolderNotFound from '@components/errors/foldernotfound';
 import { api } from '@utils/api';
 import { useSession } from 'next-auth/react';
 import type { FC } from 'react';
 
 const FolderView: FC<{ folder_id: string }> = ({ folder_id }) => {
-  const { data: session, status } = useSession();
+  const { data: session } = useSession();
   const foundFolder = api.folder.get.useQuery({
     folder_id: folder_id,
-    // @ts-ignore
+    // @ts-ignore: checked in parent component
     userid: session?.user?.id,
   });
-  return <></>;
+
+  if (foundFolder.data?.status === 200) {
+    return (
+      <>
+        <CompHead headTitle={foundFolder.data.foundNote?.name}></CompHead>
+        <TopBar avatarUrl={session?.user?.image}></TopBar>
+        {foundFolder.data.foundNote?.name}
+      </>
+    );
+  }
+  return (
+    <>
+      <CompHead headTitle='Not Found'></CompHead>
+      <TopBar avatarUrl={session?.user?.image}></TopBar>
+      <FolderNotFound folder_id={folder_id}></FolderNotFound>
+    </>
+  );
 };
 
 export default FolderView;
