9e3d836b212501c620c9d0ecda39701a228e6bf3
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-01-08 14:31:37 +0530

    adding checks

--- src/pages/new/note.tsx
+++ src/pages/new/note.tsx
@@ -1,7 +1,28 @@
+import Loading from '@components/common/Loading';
+import TopBar from '@components/common/TopBar';
 import type { NextPage } from 'next';
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/router';
 
 const NewNote: NextPage = () => {
-  return <></>;
+  const { data: session, status } = useSession();
+  const router = useRouter();
+
+  if (status == 'authenticated') {
+    return (
+      <>
+        <TopBar avatarUrl={session.user?.image}></TopBar>
+      </>
+    );
+  } else if (status == 'unauthenticated') {
+    router.push('/signin');
+  }
+
+  return (
+    <>
+      <Loading></Loading>
+    </>
+  );
 };
 
 export default NewNote;
