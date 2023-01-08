a0360024f32b575b675f911f82b46086ffbe0942
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-01-09 01:24:53 +0530

    adding file

---
+++ src/pages/view/[folder_id]/[note_id]/index.tsx
@@ -0,0 +1,11 @@
+import CompHead from '@components/common/CompHead';
+import type { FC } from 'react';
+
+const ViewNote: FC = () => {
+  return (
+    <>
+      <CompHead headTitle='View Note'></CompHead>
+    </>
+  );
+};
+export default ViewNote;
