08c39f54e29b2ac95f0b071ec7919ea79d5e0602
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-01-08 14:33:20 +0530

    adding checks

--- src/pages/new/folder.tsx
+++ src/pages/new/folder.tsx
@@ -1,7 +1,30 @@
+import CompHead from '@components/common/CompHead';
+import Loading from '@components/common/Loading';
+import TopBar from '@components/common/TopBar';
 import type { NextPage } from 'next';
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/router';
 
 const NewFolder: NextPage = () => {
-  return <></>;
+  const { data: session, status } = useSession();
+  const router = useRouter();
+
+  if (status == 'authenticated') {
+    return (
+      <>
+        <CompHead headTitle='New Folder'></CompHead>
+        <TopBar avatarUrl={session.user?.image}></TopBar>
+      </>
+    );
+  } else if (status == 'unauthenticated') {
+    router.push('/signin');
+  }
+
+  return (
+    <>
+      <Loading></Loading>
+    </>
+  );
 };
 
 export default NewFolder;
