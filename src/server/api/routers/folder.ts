798daff89c290010b509511c418e508fb672e0da
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-01-08 22:00:41 +0530

    adding folder router

---
+++ src/server/api/routers/folder.ts
@@ -0,0 +1,26 @@
+import { z } from 'zod';
+import { prisma } from 'src/server/db';
+import { createTRPCRouter, protectedProcedure } from '../trpc';
+
+export const folderRouter = createTRPCRouter({
+  create: protectedProcedure
+    .input(
+      z.object({
+        folderName: z.string(),
+        folderDesc: z.string().nullable(),
+        folderColor: z.string(),
+      })
+    )
+    .mutation(async ({ input }) => {
+      const folderCreated = await prisma.notesFolder.create({
+        data: {
+          name: input.folderName,
+          description: input.folderDesc,
+          color: input.folderColor,
+        },
+      });
+      return {
+        folder_id: folderCreated.id,
+      };
+    }),
+});
