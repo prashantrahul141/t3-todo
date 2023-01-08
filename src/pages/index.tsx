1b6e9a2b557c505194af6629e9fdea379d7fb047
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-01-08 20:36:02 +0530

    removing unused imports

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,9 @@
 import { type NextPage } from 'next';
 import CompHead from '@components/common/CompHead';
 import TopBar from '@components/common/TopBar';
-// import Link from "next/link";
 import { useSession } from 'next-auth/react';
 import Welcome from '@components/home/welcome';
 import Loading from '@components/common/Loading';
-
-// import { api } from "../utils/api";
 
 const Home: NextPage = () => {
   const { data: session, status } = useSession();
