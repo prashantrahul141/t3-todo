612aabea867504b510b0a80f47e0e495de6df999
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-01-08 14:38:36 +0530

    adding file

---
+++ src/pages/settings/index.tsx
@@ -0,0 +1,30 @@
+import CompHead from '@components/common/CompHead';
+import Loading from '@components/common/Loading';
+import TopBar from '@components/common/TopBar';
+import type { NextPage } from 'next';
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/router';
+
+const Settings: NextPage = () => {
+  const { data: session, status } = useSession();
+  const router = useRouter();
+
+  if (status == 'authenticated') {
+    return (
+      <>
+        <CompHead headTitle='Settings'></CompHead>
+        <TopBar avatarUrl={session.user?.image}></TopBar>
+      </>
+    );
+  } else if (status == 'unauthenticated') {
+    router.push('/signin');
+  }
+
+  return (
+    <>
+      <Loading></Loading>
+    </>
+  );
+};
+
+export default Settings;
