c4ce3654c1b353b77c97f0e0a67499993c64443c
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-01-09 02:29:32 +0530

    adding text and button

--- src/components/errors/foldernotfound.tsx
+++ src/components/errors/foldernotfound.tsx
@@ -1,6 +1,18 @@
+import Link from 'next/link';
 import type { FC } from 'react';
 
 const FolderNotFound: FC<{ folder_id: string }> = ({ folder_id }) => {
-  return <>{folder_id} not found.</>;
+  return (
+    <>
+      <div className='absolute top-1/3 left-1/2 -translate-x-1/2 text-center'>
+        <p className=' font-lato text-xl'>
+          Folder with id <i>&apos;{folder_id}&apos;</i> &nbsp; not found {':('}
+        </p>
+        <Link href='/new/folder'>
+          <button className='btn-signin mt-16 w-max'>Create New Folder</button>
+        </Link>
+      </div>
+    </>
+  );
 };
 export default FolderNotFound;
