92308580b7d093f853f6a68bdae4243a364bb103
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-01-08 17:42:19 +0530

    using new folder form

--- src/pages/new/folder.tsx
+++ src/pages/new/folder.tsx
@@ -1,6 +1,7 @@
 import CompHead from '@components/common/CompHead';
 import Loading from '@components/common/Loading';
 import TopBar from '@components/common/TopBar';
+import FolderForm from '@components/forms/newfolderform';
 import type { NextPage } from 'next';
 import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/router';
@@ -9,11 +10,21 @@ const NewFolder: NextPage = () => {
   const { data: session, status } = useSession();
   const router = useRouter();
 
+  const createFolder = (folderName: string, folderColor: string) => {
+    console.log(folderName, folderColor);
+  };
+
   if (status == 'authenticated') {
     return (
       <>
         <CompHead headTitle='New Folder'></CompHead>
         <TopBar avatarUrl={session.user?.image}></TopBar>
+        <FolderForm
+          titleText='Create New Folder'
+          folderName={undefined}
+          folderColor={undefined}
+          callBackFunc={createFolder}
+          buttonText='Create Folder'></FolderForm>
       </>
     );
   } else if (status == 'unauthenticated') {
