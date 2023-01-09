8298542f505ce73afdb7374e9621229330b0da5b
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-01-09 12:22:41 +0530

    div for hover effeect

--- src/components/home/foldersview.tsx
+++ src/components/home/foldersview.tsx
@@ -12,18 +12,20 @@ const FoldersView: FC = () => {
             return (
               <div
                 key={eachFolder.id}
-                className='grid items-center justify-center rounded-xl py-2 transition-all duration-200 hover:scale-105 hover:bg-themePrimary-100/10'>
+                className='grid items-center justify-center'>
                 <Link href={`/view/${eachFolder.id}`}>
-                  <svg
-                    className='h-[70px] w-[70px] sm:h-[90px] sm:w-[90px]'
-                    fill={eachFolder.color}
-                    xmlns='http://www.w3.org/2000/svg'
-                    viewBox='0 0 512 512'>
-                    <path d='M464 96h-192l-64-64h-160C21.5 32 0 53.5 0 80V160h512V144C512 117.5 490.5 96 464 96zM0 432C0 458.5 21.5 480 48 480h416c26.5 0 48-21.5 48-48V192H0V432z' />
-                  </svg>
-                  <span className=' font-lato text-base sm:text-lg'>
-                    {eachFolder.name}
-                  </span>
+                  <div className='rounded-xl py-2 px-4 transition-all duration-200 hover:scale-105 hover:bg-themePrimary-100/10'>
+                    <svg
+                      className='h-[70px] w-[70px] sm:h-[90px] sm:w-[90px]'
+                      fill={eachFolder.color}
+                      xmlns='http://www.w3.org/2000/svg'
+                      viewBox='0 0 512 512'>
+                      <path d='M464 96h-192l-64-64h-160C21.5 32 0 53.5 0 80V160h512V144C512 117.5 490.5 96 464 96zM0 432C0 458.5 21.5 480 48 480h416c26.5 0 48-21.5 48-48V192H0V432z' />
+                    </svg>
+                    <span className=' font-lato text-base sm:text-lg'>
+                      {eachFolder.name}
+                    </span>
+                  </div>
                 </Link>
               </div>
             );
