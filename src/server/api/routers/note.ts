acb45b0544637f9fc5862ef418c226d4c7d240be
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-01-09 21:39:50 +0530

    addiing note.get query

--- src/server/api/routers/note.ts
+++ src/server/api/routers/note.ts
@@ -3,6 +3,45 @@ import { prisma } from 'src/server/db';
 import { createTRPCRouter, protectedProcedure } from '../trpc';
 
 export const noteRouter = createTRPCRouter({
+  get: protectedProcedure
+    .input(
+      z.object({
+        note_id: z.string(),
+        folder_id: z.string(),
+        userid: z.string(),
+      })
+    )
+    .query(async ({ input }) => {
+      const foundNote = await prisma.note.findUnique({
+        where: {
+          id: input.note_id,
+        },
+        include: {
+          NotesFolder: {
+            include: {
+              User: true,
+            },
+          },
+          tasks: true,
+        },
+      });
+
+      if (
+        foundNote !== null &&
+        foundNote.NotesFolder?.id === input.folder_id &&
+        foundNote.NotesFolder.userId === input.userid
+      ) {
+        return {
+          status: 200,
+          foundNote: foundNote,
+        };
+      }
+
+      return {
+        status: 404,
+      };
+    }),
+
   create: protectedProcedure
     .input(
       z.object({
