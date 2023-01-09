ea41520c0de76584adf2fd04299f8c4c97aa9e4b
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-01-09 21:39:06 +0530

    adding file

---
+++ src/components/errors/notenotfound.tsx
@@ -0,0 +1,16 @@
+import Link from 'next/link';
+import type { FC } from 'react';
+
+const NoteNotFound: FC = () => {
+  return (
+    <>
+      <div className='absolute top-1/3 left-1/2 -translate-x-1/2 text-center'>
+        <p className=' font-lato text-xl'>Note not found {':('}</p>
+        <Link href='/new/note'>
+          <button className='btn-signin mt-16 w-max'>Create New Note</button>
+        </Link>
+      </div>
+    </>
+  );
+};
+export default NoteNotFound;
