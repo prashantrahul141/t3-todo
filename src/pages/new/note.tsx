69bd2319388d57ecd7e296d2c7a394c6a1cd4fe6
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-01-09 15:34:28 +0530

    using noteform component to create notes

--- src/pages/new/note.tsx
+++ src/pages/new/note.tsx
@@ -1,6 +1,8 @@
 import CompHead from '@components/common/CompHead';
 import Loading from '@components/common/Loading';
 import TopBar from '@components/common/TopBar';
+import NoteForm from '@components/forms/noteform';
+import { api } from '@utils/api';
 import type { NextPage } from 'next';
 import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/router';
@@ -9,10 +11,34 @@ const NewNote: NextPage = () => {
   const { data: session, status } = useSession();
   const router = useRouter();
 
+  const mutation = api.note.create.useMutation();
+
+  const createNote = async (
+    noteName: string,
+    noteFolderId: string,
+    noteColor: string
+  ) => {
+    const createdNoteId = await mutation.mutateAsync({
+      folderId: noteFolderId,
+      noteName: noteName,
+      color: noteColor,
+      userId: session?.user?.id || '#',
+    });
+
+    router.push(`/view/${noteFolderId}/${createdNoteId?.newNoteId}`);
+  };
+
   if (status == 'authenticated') {
     return (
       <>
         <CompHead headTitle='New Note'></CompHead>
+        <NoteForm
+          titleText='Create New Note'
+          noteName={undefined}
+          noteFolderName={undefined}
+          noteColor={undefined}
+          callBackFunc={createNote}
+          buttonText='Create Note'></NoteForm>
         <TopBar avatarUrl={session.user?.image}></TopBar>
       </>
     );
