548a1792a5695d1b33c3e4970d6d347957ede428
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-01-09 20:42:55 +0530

    adding rounded for consistency

--- src/components/common/ErrorAlert.tsx
+++ src/components/common/ErrorAlert.tsx
@@ -2,7 +2,7 @@ import type { FC } from 'react';
 
 const ErrorAlert: FC<{ text: string }> = ({ text }) => {
   return (
-    <div className='alert alert-error absolute bottom-12 left-1/2 w-full max-w-sm -translate-x-1/2 shadow-lg'>
+    <div className='alert alert-error absolute bottom-12 left-1/2 w-full max-w-sm -translate-x-1/2 rounded shadow-lg'>
       <div>
         <svg
           xmlns='http://www.w3.org/2000/svg'
