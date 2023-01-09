976cd4d2e8998f0761166227bf3938d8dc06552f
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-01-09 21:40:37 +0530

    adding page jsx

--- src/pages/view/[folder_id]/[note_id]/index.tsx
+++ src/pages/view/[folder_id]/[note_id]/index.tsx
@@ -1,10 +1,41 @@
-import CompHead from '@components/common/CompHead';
+import Loading from '@components/common/Loading';
+import NoteView from '@components/views/noteview';
+import type { NextPageContext } from 'next';
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/router';
 import type { FC } from 'react';
 
-const ViewNote: FC = () => {
+export const getServerSideProps = async (context: NextPageContext) => {
+  const { query } = context;
+  const { folder_id, note_id } = query;
+  return {
+    props: {
+      folder_id: folder_id || '#',
+      note_id: note_id || '#',
+    },
+  };
+};
+
+const ViewNote: FC<{ folder_id: string; note_id: string }> = ({
+  folder_id,
+  note_id,
+}) => {
+  const { status } = useSession();
+  const router = useRouter();
+
+  if (status === 'authenticated') {
+    return (
+      <>
+        <NoteView folder_id={folder_id} note_id={note_id}></NoteView>
+      </>
+    );
+  } else if (status === 'unauthenticated') {
+    router.push('/signin');
+  }
+
   return (
     <>
-      <CompHead headTitle='View Note'></CompHead>
+      <Loading></Loading>
     </>
   );
 };
