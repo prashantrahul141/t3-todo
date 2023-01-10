22bae2f59eff4ae8ab2f3008d998913dca4029b0
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-01-11 00:49:05 +0530

    adding google signin button

--- src/components/forms/signinform.tsx
+++ src/components/forms/signinform.tsx
@@ -5,8 +5,11 @@ const CompSigninForm: FC = () => {
   // basic sign in form
   return (
     <div className='absolute left-1/2 top-1/2 w-full -translate-x-1/2 -translate-y-1/2  p-4 sm:w-96'>
-      <button className='btn-signin' onClick={() => signIn('github')}>
+      <button className='btn-signin mt-16' onClick={() => signIn('github')}>
         Github
+      </button>
+      <button className='btn-signin my-2' onClick={() => signIn('google')}>
+        Google
       </button>
       <button className='btn-signin' onClick={() => signIn('discord')}>
         Discord
