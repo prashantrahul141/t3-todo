811f3178ffaeaf607d2743131ba14c77cc514746
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-01-10 21:25:35 +0530

    linking manifest

--- src/components/common/CompHead.tsx
+++ src/components/common/CompHead.tsx
@@ -10,6 +10,7 @@ const CompHead: FC<{ headTitle: string | undefined }> = ({
       {headTitle === undefined && <title>{`Todo`}</title>}
       {headTitle !== undefined && <title>{`Todo | ${headTitle}`}</title>}
       <meta name='description' charSet='UTF-8' content='Simple todo app' />
+      <link rel='manifest' href='/manifest.json'></link>
     </Head>
   );
 };
