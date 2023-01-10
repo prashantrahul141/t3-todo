572d2348e656250cc700d6da624dedcec9db67ca
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-01-11 01:41:46 +0530

    adding dark reader lock

--- src/components/common/CompHead.tsx
+++ src/components/common/CompHead.tsx
@@ -11,6 +11,7 @@ const CompHead: FC<{ headTitle: string | undefined }> = ({
       {headTitle !== undefined && <title>{`Todo | ${headTitle}`}</title>}
       <meta name='description' charSet='UTF-8' content='Simple todo app' />
       <link rel='manifest' href='/manifest.json'></link>
+      <meta name='darkreader-lock' />
     </Head>
   );
 };
