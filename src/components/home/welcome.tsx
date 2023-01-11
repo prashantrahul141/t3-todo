360485899088c87e8f86ab9e25d17a7c3f64549f
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-01-11 15:33:28 +0530

    fixing styling for welcome page

--- src/components/home/welcome.tsx
+++ src/components/home/welcome.tsx
@@ -9,10 +9,10 @@ const Welcome: NextPage = () => {
       {/* Top text */}
       <CompHead headTitle={undefined}></CompHead>
       <div className='absolute top-1/4 left-1/2 w-max -translate-x-1/2'>
-        <span className='float-left font-spaceGrotesk text-6xl tracking-widest text-themePrimary-300 sm:text-8xl sm:tracking-wider'>
+        <span className='float-left font-spaceGrotesk text-7xl tracking-widest text-themePrimary-300 sm:text-[6.6rem] sm:tracking-wider'>
           T3 TODO
         </span>
-        <span className='float-left clear-left pl-2 font-robotoLight text-lg tracking-wider text-themePrimary-100 sm:pl-2.5 sm:text-2xl sm:tracking-widest'>
+        <span className='float-left clear-left pl-1 font-robotoLight text-xl tracking-wider text-themePrimary-100 sm:pl-2.5 sm:text-2xl sm:tracking-widest'>
           The easiest way to maintain tasks
         </span>
       </div>
