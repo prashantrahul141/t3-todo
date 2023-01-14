97acf44646b88c6dfa8456cf37f0cef703d0a927
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-01-14 14:18:17 +0530

    adding embed info

--- src/components/common/CompHead.tsx
+++ src/components/common/CompHead.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import type { FC } from 'react';
+import image from 'public/static/meta/screenshot-1.png';
 
 const CompHead: FC<{ headTitle: string | undefined }> = ({
   headTitle = undefined,
@@ -12,6 +13,14 @@ const CompHead: FC<{ headTitle: string | undefined }> = ({
       <meta name='description' charSet='UTF-8' content='Simple todo app' />
       <link rel='manifest' href='/manifest.json'></link>
       <meta name='darkreader-lock' />
+      <meta content='T3 Todo' property='og:title' />
+      <meta
+        content='The easiest way to maintain tasks.'
+        property='og:description'
+      />
+      <meta content='https://t3-todo.up.railway.app' property='og:url' />
+      <meta content={image.src} property='og:image' />
+      <meta content='#c393f6' data-react-helmet='true' name='theme-color' />
     </Head>
   );
 };
